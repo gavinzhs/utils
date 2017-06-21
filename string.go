cf8709ed45ec8b6ec0de7f0062f0a2fe757206f2
zhengsai <gavinzhs@gmail.com>
2017-06-21 17:30:45 +0800

    add reversestring

---
+++ string.go
@@ -0,0 +1,10 @@
+package utils
+
+func reverseString(ori string) string {
+	runes := []rune(ori)
+	for from, to := 0, len(runes) - 1; from < to; from, to = from + 1, to - 1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+
+	return string(runes)
+}
